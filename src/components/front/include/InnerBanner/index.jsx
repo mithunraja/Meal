c24f5cc4666307c9c6c8de1d991727359ded6675
mithunraja <mithun.dey1979@gmail.com>
2024-11-04 12:31:32 +0530

    second com

--- src/components/front/include/InnerBanner/index.jsx
+++ src/components/front/include/InnerBanner/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import style from "./index.module.scss";
+import banner from "@/assets/front/images/inner-ban.jpg";
 
-const InnerBanner = () => {
+const InnerBanner = ({ title }) => {
   return (
-    <>
-      <div className={style.banner}></div>
-    </>
+    <section
+      className={`d-flex align-items-center justify-content-center ${style.InnerBanner}`}
+      style={{ backgroundImage: `url(${banner.src})` }}
+    >
+      <h1>{title}</h1>
+    </section>
   );
 };
 
